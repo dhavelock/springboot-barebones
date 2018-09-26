8006d85c63ca6c63c2d4e12f73a8063ca30d1969
Dylan Havelock <dylanhavelock@Dylans-MacBook-Pro-2.local>
2018-09-26 11:55:57 -0400

    MySQL example moved to HelloController.java

--- src/main/java/ca/mcgill/ecse321/Application.java
+++ src/main/java/ca/mcgill/ecse321/Application.java
@@ -2,32 +2,11 @@ package ca.mcgill.ecse321;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.hibernate.Session;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-
-        /* Connect to the DB and begin an atomic operation */
-        Session session = HibernateUtil.getSession();
-        session.beginTransaction();
-
-        /* Create an example student */
-        Hello hello = new Hello(2, "Hello again!");
-
-        /* Write the student to the DB by committing the transaction */
-        session.saveOrUpdate(hello);
-
-        session.getTransaction().commit();
-
-        session.close();
-
-        //HibernateUtil.closeSession();
-
-        System.out.println("close");
-
-        /* Back to the spring application. The above is to test Hibernate */
         SpringApplication.run(Application.class, args);
     }
 }
--- src/main/java/ca/mcgill/ecse321/HelloController.java
+++ src/main/java/ca/mcgill/ecse321/HelloController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import org.hibernate.Session;
+
 @RestController
 public class HelloController {
 
@@ -13,7 +15,23 @@ public class HelloController {
 
     @RequestMapping("/hello")
     public Hello hello(@RequestParam(value="name", defaultValue="World") String name) {
-        return new Hello(counter.incrementAndGet(),
-                            String.format(template, name));
+        Hello retHello = new Hello(counter.incrementAndGet(), String.format(template, name));
+
+        /* Connect to the DB and begin an atomic operation */
+        Session session = HibernateUtil.getSession();
+        session.beginTransaction();
+
+        /* Write the student to the DB by committing the transaction */
+        session.saveOrUpdate(retHello);
+
+        session.getTransaction().commit();
+
+        session.close();
+
+        //HibernateUtil.closeSession();
+
+        System.out.println("close");
+
+        return retHello;
     }
 }
